4401a60a4df32f2df473c595d1989c727db0239a
Brandon Hernandez Torres <brandonhernandeztorres@MacBook-Air-de-Brandon.local>
2021-06-22 16:35:53 -0500

    :hammer: UPDATE: Se implementa paginación en la sección de api

--- src/pages/RickandMorty/RickAndMorty.js
+++ src/pages/RickandMorty/RickAndMorty.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Alert } from 'react-bootstrap';
 /** COMPONENTES INTERNOS */
 import NavbarComponent from '../../Components/NavBar/NavbarComponent';
 /** COMPONENTES EXTERNOS */
 import FlatList from 'flatlist-react';
-import { Container, Card, Pagination } from 'react-bootstrap';
+import { Container, Card, Button, Toast, Form, Pagination } from 'react-bootstrap';
 /** UTILS */
 import { peticionGenerica } from '../../utils/Api';
 /** ESTILOS */
@@ -13,6 +12,7 @@ import './RickAndMorty.css';
 const RickAndMorty = (props) => {
 
     const [esAlerta, setEsAlerta] = useState(false);
+    const [mensajeAlerta, setMensajeAlerta] = useState('');
     const [listaPersonajes, setListaPersonajes] = useState([]);
     const [page, setPage] = useState(1);
     const [paginaMaxima, setPaginaMaxima] = useState(100);
@@ -35,14 +35,16 @@ const RickAndMorty = (props) => {
 
     const cambiarPagina = async (pagina) => {
         try {
+            setEsAlerta(true);
+            setMensajeAlerta(`Se cargo la página ${pagina}`);
             setPage(pagina);
             let obtenido = await peticionGenerica(URL_PERSONAJES_API + pagina, 'GET', false, {}, false, {});
             console.log('obtenido:: ', obtenido.data.results[0]);
             setListaPersonajes(obtenido.data.results);
         } catch (error) {
             console.log('error-obtenerDatos:: ', error);
-        }
-    }
+        };
+    };
 
     const renderPersonaje = (item) => (
         <Card border="info" >
@@ -62,7 +64,6 @@ const RickAndMorty = (props) => {
             <NavbarComponent
                 props={props}
             />
-
             <Container>
                 <div className="contenedor-cards" >
                     <FlatList
@@ -72,32 +73,27 @@ const RickAndMorty = (props) => {
                         displayGrid
                     />
                 </div>
+
                 <div className="contenedor-paginacion" >
                     <Pagination>
-                        <Pagination.Item key={page - 1} active={page - 1 === page}  disabled={ page-1 === 0 } onClick={() => cambiarPagina(page-1) }>
-                            {page - 1}
-                        </Pagination.Item>
-                        <Pagination.Item key={page} active={page === page}>
-                            {page}
-                        </Pagination.Item>
-                        <Pagination.Item key={page + 1} active={page + 1 === page} disabled={page+1 === paginaMaxima} onClick={() => cambiarPagina(page+1)} >
-                            {page + 1}
-                        </Pagination.Item>
-                        <Pagination.Item key={page + 1} active={page + 2 === page} disabled={page+2 === paginaMaxima} onClick={() => cambiarPagina(page+2)} >
-                            {page + 2}
-                        </Pagination.Item>
-                        <Pagination.Item key={page + 3} active={page + 3 === page} disabled={page+3 === paginaMaxima} onClick={() => cambiarPagina(page+3)} >
-                            {page + 3}
-                        </Pagination.Item>
+                        <Pagination.Prev onClick={() => cambiarPagina(page - 1)} disabled={page === 1} />
+                        <Pagination.Item active>{page}</Pagination.Item>
+                        <Pagination.Next onClick={() => cambiarPagina(page + 1)} disabled={page === paginaMaxima} />
                     </Pagination>
                 </div>
             </Container>
 
-            <div style={{ position: 'absolute' }} >
-                <Alert variant='danger' show={esAlerta} dismissible={true} onClose={() => setEsAlerta(false)} >
-                    Usuario y/o contraseña incorrectos
+            {/* <div style={{ position: 'relative'}} >
+                <Alert variant='success' show={esAlerta} dismissible={true} onClose={() => setEsAlerta(false)} >
+                    {mensajeAlerta}
                 </Alert>
-            </div>
+            </div> */}
+            <Toast show={esAlerta} onClose={() => setEsAlerta(false)} delay={2000} autohide >
+                <Toast.Header>
+                    Carga exitosa!
+                </Toast.Header>
+                <Toast.Body>{mensajeAlerta}</Toast.Body>
+            </Toast>
         </div>
     )
 };
